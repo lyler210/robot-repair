776b3e3d3a25934bd4e1a9abd95866bd1b5719b8
Lyle Racho <lyleracho@Lyles-MacBook-Pro.local>
2026-04-15 15:22:30 -0700

    Expanded map. Updated PlayerMovement and InputActions to WASD.

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        Vector2 move = MoveAction.ReadValue<Vector2>(); // Gets current value of MoveAction
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.01f; // Moves in direction according to move action
         transform.position = position;
     }
 }
