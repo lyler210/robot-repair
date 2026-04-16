01596f1111f46df48a6edcf431f890b0a2f3ed40
Lyle Racho <lyleracho@unknown46645fc74111.home>
2026-04-16 00:37:23 -0700

    Added a bunch of stuff.

---
+++ Assets/_2DAdventureGame/Scripts/EnemyController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+   	// Public variables
+   	public float speed;
+   	public bool vertical;
+   	public float changeTime = 3.0f;
+    Animator animator;
+  
+   	// Private variables
+    Rigidbody2D rigidbody2d;
+   	float timer;
+   	int direction = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+   	}
+
+   	// Update is called every frame
+   	void Update()
+   	{
+       	timer-= Time.deltaTime;
+
+      		if (timer < 0)
+      		{
+        		direction = -direction;
+        		timer = changeTime;
+      		}
+   	}
+
+  	// FixedUpdate has the same call rate as the physics system
+  	void FixedUpdate()
+  	{    
+       	Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            position.y = position.y + speed * direction * Time.deltaTime;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + speed * direction * Time.deltaTime;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+
+       	rigidbody2d.MovePosition(position);
+  	}
+
+   	void OnTriggerEnter2D(Collider2D other)
+   	{
+       	PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+       	if (player != null)
+       	{
+           		player.ChangeHealth(-1);
+       	}
+   	}
+}
--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -3,19 +3,66 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables related to player character movement
     public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>(); // Gets current value of MoveAction
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.01f; // Moves in direction according to move action
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+        //Debug.Log(move);
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
+    }
+
+    // FixedUpdate has the same call rate as the physics system
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
